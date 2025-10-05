710fac7f39eda1a544df44040c64332803dfd8f6
Christian21321 <164412626+Christian21321@users.noreply.github.com>
2025-10-05 16:24:33 -0300

    Frontend provisional

---
+++ WITW/demo/src/main/java/com/WITW/demo/WitwApplication.java
@@ -0,0 +1,13 @@
+package com.WITW.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitwApplication.class, args);
+	}
+
+}
---
+++ WITW/demo/src/test/java/com/WITW/demo/WitwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WITW.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
